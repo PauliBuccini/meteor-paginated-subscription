9f376dd342eef3cb523e47ec5487f18fba21c3ca
Pauli B <pauli.b@outlook.com>
2013-09-27 15:46:33 +0300

    Update paginated_subscription.js

--- paginated_subscription.js
+++ paginated_subscription.js
@@ -1,9 +1,19 @@
 PaginatedSubscriptionHandle = function(perPage) {
+
   this.perPage = perPage;
+  //  Initinal skip number same as per page
+  this.skipNumber = perPage;
   this._limit = perPage;
   this._limitListeners = new Deps.Dependency();
   this._loaded = 0;
   this._loadedListeners = new Deps.Dependency();
+  //  How many skipped items
+  this._skip = 0;
+  //  How many times skipped
+  this._skipTimes = 0;
+  this._skipTimesListeners = new Deps.Dependency();
+  //  How many times loaded, as a helper for skip method
+  this._loadedTimes = 0;
 }
 
 PaginatedSubscriptionHandle.prototype.loaded = function() {
@@ -17,6 +27,7 @@ PaginatedSubscriptionHandle.prototype.limit = function() {
 }
 
 PaginatedSubscriptionHandle.prototype.ready = function() {
+  console.log('check i f ready')
   return this.loaded() === this.limit();
 }
 
@@ -28,18 +39,36 @@ PaginatedSubscriptionHandle.prototype.loading = function() {
 PaginatedSubscriptionHandle.prototype.loadNextPage = function() {
   this._limit += this.perPage;
   this._limitListeners.changed();
+  // how many Loads
+  this._loadedTimes ++;
+  this._skipTimes = this._loadedTimes - 1;
 }
 
 PaginatedSubscriptionHandle.prototype.done = function() {
   // XXX: check if subs that are canceled before they are ready ever fire ready?
   // if they do we need to increase loaded by perPage, not set it to limit
+  //  dadeta skip cancelingas
   this._loaded = this._limit;
+  this._skipTimes = this._skipTimes;
   this._loadedListeners.changed();
+  this._skipTimesListeners.changed();
 }
 
 PaginatedSubscriptionHandle.prototype.reset = function() {
   this._limit = this.perPage;
   this._limitListeners.changed();
+}
+//  How many items to skip
+PaginatedSubscriptionHandle.prototype.skip = function() {
+ this._skipTimesListeners.depend();
+  this._skip = this._skipTimes * this.skipNumber || 0;
+  return this._skip;
+}
+
+//  Load skipped files
+PaginatedSubscriptionHandle.prototype.loadPreviuosPage = function() {
+  this._skipTimes --;
+  this._skipTimesListeners.changed();
 }
 
 
@@ -47,19 +76,18 @@ PaginatedSubscriptionHandle.prototype.reset = function() {
 Meteor.subscribeWithPagination = function (/*name, arguments, perPage */) {
   var args = Array.prototype.slice.call(arguments, 0);
   var perPage = args.pop();
-  
   var handle = new PaginatedSubscriptionHandle(perPage);
-  
+
   Meteor.autorun(function() {
     var ourArgs = _.map(args, function(arg) {
       return _.isFunction(arg) ? arg() : arg;
     });
-    
+
     var subHandle = Meteor.subscribe.apply(this, ourArgs.concat([
       handle.limit(), function() { handle.done(); }
     ]));
     handle.stop = subHandle.stop;
   });
-  
+
   return handle;
 }
