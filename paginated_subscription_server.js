8dcbb15d9493d3b06c772cb55c0a1d7f03c0c53c
Pauli B <pauli.buccini@gmail.com>
2014-05-08 09:13:00 +0100

    update

--- paginated_subscription_server.js
+++ paginated_subscription_server.js
@@ -3,7 +3,7 @@ Meteor.methods({
     // console.log(subName, query)
     if (subName)
       var count = Collection[subName].find(query,{fields: {_id: 1}}).count() || 0;
-    console.log(count)
+    // console.log(count)
     return count ? count : 1;
   }
 })
